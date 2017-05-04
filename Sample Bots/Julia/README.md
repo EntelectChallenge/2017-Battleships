bb1af523ac5072055ab47a46cdb663e32609ff52
Marius Kruger <amanic@gmail.com>
2017-05-05 00:59:11 +0200

    implement phase detection \o/

--- Sample Bots/Julia/README.md
+++ Sample Bots/Julia/README.md
@@ -7,3 +7,6 @@ Just uses julia standard library
 ### Installing Julia
 
 See https://julialang.org/downloads/
+
+### Supported/Tested Julia versions
+* 0.5.1 (Ubuntu 16.04.2 LTS)
--- Sample Bots/Julia/main.jl
+++ Sample Bots/Julia/main.jl
@@ -1,1 +1,34 @@
-print("Hello Julia")
+import JSON
+
+commandFileName = "command.txt"
+placeShipFileName = "place.txt"
+#stateFileName = "test_data/Phase 1 - Round 0/state.json"
+stateFileName = "test_data/Phase 2 - Round 1/state.json"
+#stateFileName = "state.json"
+
+state = JSON.parsefile(stateFileName)
+
+#print(state)
+
+function makeYourMove()
+  phase = state["Phase"]
+  println("* Phase $phase")
+  if phase == 1
+    deployShips()
+  elseif phase == 2
+    println("Bombs away!..")
+    bombsAway()
+  else
+    throw(Exception("Unsupported phase: '$phase'"))
+  end
+end
+
+function deployShips()
+  println("* Deploying ships..")
+end
+
+function bombsAway()
+  println("* Bombs away!")
+end
+
+makeYourMove()
